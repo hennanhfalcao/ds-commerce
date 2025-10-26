c4cb83b9ffc5fdcdca7cd7b863beecf20a9af2bb
Hennan H. Falcão <hennan.falcao@cs.unipe.edu.br>
2025-10-26 16:01:47 -0300

    :refactor: UserDetails and GrantedAuthority interface implemented

--- src/main/java/com/devsuperior/dscommerce/entities/Role.java
+++ src/main/java/com/devsuperior/dscommerce/entities/Role.java
@@ -1,10 +1,13 @@
 package com.devsuperior.dscommerce.entities;
 
 import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
 
+
+@SuppressWarnings("serial")
 @Entity
 @Table(name="tb_role")
-public class Role {
+public class Role implements GrantedAuthority {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -27,6 +30,7 @@ public class Role {
         this.id = id;
     }
 
+    @Override
     public String getAuthority() {
         return authority;
     }
